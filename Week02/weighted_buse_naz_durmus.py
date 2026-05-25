d95189e22c768b65871dce87d943052dea44d8d6
busedrms <159904093+busedrms@users.noreply.github.com>
2026-05-25 16:29:22 +0300

    Update weighted_buse_naz_durmus.py

--- Week02/weighted_buse_naz_durmus.py
+++ Week02/weighted_buse_naz_durmus.py
@@ -3,4 +3,4 @@ import random
 def weighted_srs(data: list, n: int, weights: list, with_replacement: bool = False):
     if with_replacement:
         return random.choices(data, weights=weights, k=n)
-    return random.sample(data, k=n, counts=[round(w * 1000) for w in weights])
+    return random.sample(data, k=n, counts=weights)
