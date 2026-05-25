add54d8f41eb6770c8dfbb9b9697ffc5177bb87f
busedrms <159904093+busedrms@users.noreply.github.com>
2026-05-25 15:56:22 +0300

    Update weighted_buse_naz_durmus.py

--- Week02/weighted_buse_naz_durmus.py
+++ Week02/weighted_buse_naz_durmus.py
@@ -3,6 +3,10 @@ import random
 def weighted_srs(data: list, n: int, weights: list, with_replacement: bool):
     if with_replacement:
         return random.choices(data, weights=weights, k=n)
-
-    expanded = sum([[elem] * int(w) for elem, w in zip(data, weights)], [])
-    return random.sample(expanded, k=n)
+    secilenler = []
+    
+    while len(secilenler) < n:
+        aday = random.choices(data, weights=weights, k=1)[0]
+        if aday not in secilenler:
+            secilenler.append(aday)
+              return secilenler
