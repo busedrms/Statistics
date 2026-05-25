769c04bc54f5c0b39c90b8dc616b2a5f47d6dee3
busedrms <159904093+busedrms@users.noreply.github.com>
2026-05-25 16:11:36 +0300

    Update weighted_buse_naz_durmus.py

--- Week02/weighted_buse_naz_durmus.py
+++ Week02/weighted_buse_naz_durmus.py
@@ -1,6 +1,8 @@
-mport random
+import random
 
 def weighted_srs(data: list, n: int, weights: list, *, with_replacement: bool = False):
     if with_replacement:
         return random.choices(data, weights=weights, k=n)
-    return random.sample(sum([[e] * int(w * 100) for e, w in zip(data, weights)], []), k=n)
+    
+    expanded = sum([[elem] * int(w * 1000) for elem, w in zip(data, weights)], [])
+    return random.sample(expanded, k=n)
